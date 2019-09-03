f542559ebb5a11250cc067bf84b18dd8469fae05
Anton <anton@adc.sh>
2019-09-03 14:08:50 +0300

    correct values

--- src/index.js
+++ src/index.js
@@ -4,21 +4,25 @@
  * @param {!HTMLElement} element The source element.
  */
 export default function(obj, element) {
-  const bb = element.getBoundingClientRect()
-  const top = bb.top + window.scrollY
-  const left = bb.left + window.scrollX
-  const width = bb.width
-  const height = bb.height
+  const rect = element.getBoundingClientRect()
+  const top = rect.top + window.scrollY
+  const left = rect.left + window.scrollX
+  const width = rect.width
+  const height = rect.height
 
   const cs = getComputedStyle(element)
   const pt = parseInt(cs.paddingTop, 10)
+  const pb = parseInt(cs.paddingBottom, 10)
   const pl = parseInt(cs.paddingLeft, 10)
+  const pr = parseInt(cs.paddingRight, 10)
   const bt = parseInt(cs.borderTopWidth, 10)
   const bl = parseInt(cs.borderLeftWidth, 10)
+  const br = parseInt(cs.borderRightWidth, 10)
+  const bb = parseInt(cs.borderRightBottom, 10)
 
   obj.style.position = 'absolute'
-  obj.style.top = top + pt + bt + 'px'
-  obj.style.left = left - pl - bl + 'px'
-  obj.style.width = width + 'px'
-  obj.style.height = height + 'px'
+  obj.style.top = top + (pt + bt) + 'px'
+  obj.style.left = left + (pl + bl) + 'px'
+  obj.style.width = width - (pl + pr + bl + br) + 'px'
+  obj.style.height = height - (pt + pb + bt + bb) + 'px'
 }
